251c800fcf0fa998a111d3a03dd950e497e63942
Craig Gumbley <craiggumbley@gmail.com>
2019-09-21 22:04:26 +0100

    install polaris module

--- template/powershell-polaris/polaris.ps1
+++ template/powershell-polaris/polaris.ps1
@@ -1,3 +1,5 @@
+Import-Module -Name Polaris
+
 New-PolarisRoute -Path / -Method GET -ScriptPath $PSScriptRoot\function\handler.ps1
 
 Start-Polaris -UseJsonBodyParserMiddleware -Port $ENV:OF_PORT -Verbose
