e0b4047cc0f93825b445d24bbea2b6aeaf8c1176
Craig Gumbley <craiggumbley@gmail.com>
2019-09-22 19:55:29 +0100

    Handle GET and POST requests

--- template/powershell-polaris/function/handler.ps1
+++ template/powershell-polaris/function/handler.ps1
@@ -1,1 +1,17 @@
-$Response.Send("Hello")
+switch ($Request.Method) {
+    'GET' {
+        $Response.Send("Hello! Welcome to openfaas-powershell-polaris")
+        break
+    }
+
+    'POST' {
+        $Name = $Request.Body.Name
+        if (!$Name) {
+            $Response.SetStatusCode(400)
+            $Response.Send("Name property is missing")
+        } else {
+            $Response.Send("Hello $($Name)! Welcome to openfaas-powershell-polaris")
+        }
+        break
+    }
+}
