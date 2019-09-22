3aee13b84e022ebc955d6aeabfbc5d33a6ca8249
Craig Gumbley <craiggumbley@gmail.com>
2019-09-22 19:55:03 +0100

    Add some logic in to the polaris initializer

--- template/powershell-polaris/polaris.ps1
+++ template/powershell-polaris/polaris.ps1
@@ -1,5 +1,20 @@
 Import-Module -Name Polaris
 
-New-PolarisRoute -Path / -Method GET -ScriptPath $PSScriptRoot\function\handler.ps1
+$HandlerPath = "$PSScriptRoot\function\handler.ps1"
+$StatusHandlerPath = "$PSScriptRoot\function\status.ps1"
 
-Start-Polaris -UseJsonBodyParserMiddleware -Port $ENV:OF_PORT -Verbose
+$AppRoute = "/"
+if ($ENV:CUSTOM_ROUTE) {
+    $AppRoute = $ENV:CUSTOM_ROUTE
+}
+
+# --- Register routes
+New-PolarisRoute -Path $AppRoute -Method GET -ScriptPath $HandlerPath -Verbose
+New-PolarisRoute -Path $AppRoute -Method POST -Scriptblock $HandlerPath -Verbose
+New-PolarisRoute -Path /status -Method GET -ScriptPath $StatusHandlerPath -Verbose
+
+$App = Start-Polaris -UseJsonBodyParserMiddleware -Port $ENV:POLARIS_PORT -Verbose
+
+while($App.Listener.IsListening){
+    Wait-Event callbackcomplete
+}
