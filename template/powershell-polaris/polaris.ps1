ca7ba53f7e60d2a6222523b80df7d00361b668dd
Craig Gumbley <craiggumbley@gmail.com>
2019-09-22 20:36:05 +0100

    Set port as constant and add polaris prefix to env var

--- template/powershell-polaris/polaris.ps1
+++ template/powershell-polaris/polaris.ps1
@@ -1,11 +1,17 @@
 Import-Module -Name Polaris
 
+$PolarisPort = "8888"
 $HandlerPath = "$PSScriptRoot\function\handler.ps1"
 $StatusHandlerPath = "$PSScriptRoot\function\status.ps1"
 
 $AppRoute = "/"
-if ($ENV:CUSTOM_ROUTE) {
-    $AppRoute = $ENV:CUSTOM_ROUTE
+if ($ENV:POLARIS_CUSTOM_ROUTE) {
+    Write-Verbose -Message "Overriding default AppRoute with ${ENV:POLARIS_CUSTOM_ROUTE}"
+    $AppRoute = $ENV:POLARIS_CUSTOM_ROUTE
+
+    if (!$AppRoute.StartsWith("/")) {
+        $AppRoute = "/${AppRoute}"
+    }
 }
 
 # --- Register routes
@@ -13,7 +19,7 @@ New-PolarisRoute -Path $AppRoute -Method GET -ScriptPath $HandlerPath -Verbose
 New-PolarisRoute -Path $AppRoute -Method POST -ScriptPath $HandlerPath -Verbose
 New-PolarisRoute -Path /status -Method GET -ScriptPath $StatusHandlerPath -Verbose
 
-$App = Start-Polaris -UseJsonBodyParserMiddleware -Port $ENV:POLARIS_PORT -Verbose
+$App = Start-Polaris -UseJsonBodyParserMiddleware -Port $PolarisPort -Verbose
 
 while($App.Listener.IsListening){
     Wait-Event callbackcomplete
